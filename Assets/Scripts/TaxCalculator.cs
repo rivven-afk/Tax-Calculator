0f61fb90561f1946140b211502a210ecb888f595
gumbers6 <seth.gumbrell@education.nsw.gov.au>
2021-05-04 12:41:29 +1000

    update

--- Assets/Scripts/TaxCalculator.cs
+++ Assets/Scripts/TaxCalculator.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
 using SpeechLib;
 
 public class TaxCalculator : MonoBehaviour
@@ -8,83 +10,93 @@ public class TaxCalculator : MonoBehaviour
 
     // Variables
     bool textToSpeechEnabled = true;
+    public InputField salary;
+    public Dropdown payperiod;
+    public Text netincome;
+    public Text incometax;
+    public Text medicarelevy;
+    
 
     private void Start()
     {
-        Speak("Welcome to the A.T.O. Tax Calculator");
+        Speak("Welcom to the A.T.O tax calculator");
     }
 
-    // Run this function on the click event of your 'Calculate' button
-    public void Calculate()
-    {
-        // Initialisation of variables
-        double medicareLevyPaid = 0;
-        double incomeTaxPaid = 0;
+    // Run this function on the click event of your 'Calculate' button.
 
-        // Input
-        double grossSalaryInput = GetGrossSalary();
-        string salaryPayPeriod = GetSalaryPayPeriod();
 
-        // Calculations
-        double grossYearlySalary = CalculateGrossYearlySalary(grossSalaryInput, salaryPayPeriod);
-        double netIncome = CalculateNetIncome(grossYearlySalary, ref medicareLevyPaid, ref incomeTaxPaid);
 
-        // Output
-        OutputResults(medicareLevyPaid, incomeTaxPaid, netIncome);
-    }
+        public void Calculate()
+        {
+            // Initialisation of variables
+            double medicareLevyPaid = 0;
+            double incomeTaxPaid = 0;
 
-    private double GetGrossSalary()
-    {
-        // Get from user. E.g. input box
-        // Validate the input (ensure it is a positive, valid number)
-        double grossYearlySalary = 1000;
-        return grossYearlySalary;
-    }
+            // Input
+            double grossSalaryInput = GetGrossSalary();
+            string salaryPayPeriod = GetSalaryPayPeriod();
 
-    private string GetSalaryPayPeriod()
-    {
-        // Get from user. E.g. combobox or radio buttons
-        string salaryPayPeriod = "weekly";
-        return salaryPayPeriod;
-    }
+            // Calculations
+            double grossYearlySalary = CalculateGrossYearlySalary(grossSalaryInput, salaryPayPeriod);
+            double netIncome = CalculateNetIncome(grossYearlySalary, ref medicareLevyPaid, ref incomeTaxPaid);
 
-    private double CalculateGrossYearlySalary(double grossSalaryInput, string salaryPayPeriod)
-    {
-        // This is a stub, replace with the real calculation and return the result
-        double grossYearlySalary = 50000;
-        return grossYearlySalary;
-    }
+            // Output
+            OutputResults(medicareLevyPaid, incomeTaxPaid, netIncome);
+        }
 
-    private double CalculateNetIncome(double grossYearlySalary, ref double medicareLevyPaid, ref double incomeTaxPaid)
-    {
-        // This is a stub, replace with the real calculation and return the result
-        medicareLevyPaid = CalculateMedicareLevy(grossYearlySalary);
-        incomeTaxPaid = CalculateIncomeTax(grossYearlySalary);
-        double netIncome = 33000;        
-        return netIncome;
-    }
+        private double GetGrossSalary()
+        {
+            // Get from user. E.g. input box
+            // Validate the input (ensure it is a positive, valid number)
+            double grossYearlySalary = double.Parse(salary.text);
+            return grossYearlySalary;
+        }
 
-    private double CalculateMedicareLevy(double grossYearlySalary)
-    {
-        // This is a stub, replace with the real calculation and return the result
-        double medicareLevyPaid = 2000;        
-        return medicareLevyPaid;
-    }
+        private string GetSalaryPayPeriod()
+        {
+            // Get from user. E.g. combobox or radio buttons
+            string salaryPayPeriod = "weekly";
+            return salaryPayPeriod;
+        }
 
-    private double CalculateIncomeTax(double grossYearlySalary)
-    {
-        // This is a stub, replace with the real calculation and return the result
-        double incomeTaxPaid = 15000;
-        return incomeTaxPaid;
-    }
+        private double CalculateGrossYearlySalary(double grossSalaryInput, string salaryPayPeriod)
+        {
+            // This is a stub, replace with the real calculation and return the result
+            double grossYearlySalary = 50000;
+            return grossYearlySalary;
+        }
 
-    private void OutputResults(double medicareLevyPaid, double incomeTaxPaid, double netIncome)
-    {
-        // Output the following to the GUI
-        // "Medicare levy paid: $" + medicareLevyPaid.ToString("F2");
-        // "Income tax paid: $" + incomeTaxPaid.ToString("F2");
-        // "Net income: $" + netIncome.ToString("F2");
-    }
+        private double CalculateNetIncome(double grossYearlySalary, ref double medicareLevyPaid, ref double incomeTaxPaid)
+        {
+            // This is a stub, replace with the real calculation and return the result
+            medicareLevyPaid = CalculateMedicareLevy(grossYearlySalary);
+            incomeTaxPaid = CalculateIncomeTax(grossYearlySalary);
+            double netIncome = 33000;
+            return netIncome;
+        }
+
+        private double CalculateMedicareLevy(double grossYearlySalary)
+        {
+            // This is a stub, replace with the real calculation and return the result
+            double medicareLevyPaid = 2000;
+            return medicareLevyPaid;
+        }
+
+        private double CalculateIncomeTax(double grossYearlySalary)
+        {
+            // This is a stub, replace with the real calculation and return the result
+            double incomeTaxPaid = 15000;
+            return incomeTaxPaid;
+        }
+
+        private void OutputResults(double medicareLevyPaid, double incomeTaxPaid, double netIncome)
+        {
+            // Output the following to the GUI
+            // "Medicare levy paid: $" + medicareLevyPaid.ToString("F2");
+            // "Income tax paid: $" + incomeTaxPaid.ToString("F2");
+            // "Net income: $" + netIncome.ToString("F2");
+        }
+     
 
     // Text to Speech
     private void Speak(string textToSpeak)
